38764d767a2b23555d04c85b997801a01b27fddb
kasyapkondiparthi <srikasyap2006@gmail.com>
2026-04-23 18:36:49 +0530

    add debug console log to main entry

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+console.log("App loaded successfully");
+
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
